bba7f714fdc65bf1b11571c6f87a3521248d6949
Anton_Kiryushin <papas2290@mail.ru>
2022-10-27 22:49:23 +0200

    added test_parser.py and conftest.py

--- conftest.py
+++ conftest.py
@@ -2,12 +2,27 @@ import pytest
 from selenium import webdriver
 
 
+# from webdriver_manager.firefox import GeckoDriverManager
 
-@pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    print('\nthis is conftest')
-    browser = webdriver.Chrome()
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
+
+
+@pytest.fixture(scope='function')
+def browser(request):
+    print(request)
+    browser_name = request.config.getoption('browser_name')
+    # browser = None
+    if browser_name == 'chrome':
+        print('\nstart chrome browser for test...')
+        browser = webdriver.Chrome()
+    elif browser_name == 'firefox':
+        print('\nstart firefox browser for test...')
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError('--browser_name should be chrome or firefox')
     yield browser
-    print("\nquit browser..")
+    print('\nquit browser..')
     browser.quit()
