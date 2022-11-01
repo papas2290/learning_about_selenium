f9ba6971cc437094242540d15bf571fb6d40ef9f
Anton_Kiryushin <papas2290@mail.ru>
2022-11-01 20:08:21 +0200

    added directory language_task

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 # from webdriver_manager.firefox import GeckoDriverManager
@@ -8,19 +9,26 @@ from selenium import webdriver
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default=None,
+                     help='language')
 
 
 @pytest.fixture(scope='function')
 def browser(request):
-    print(request)
+    # print(request)
     browser_name = request.config.getoption('browser_name')
+    user_language = request.config.getoption('language')
     # browser = None
     if browser_name == 'chrome':
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         print('\nstart chrome browser for test...')
-        browser = webdriver.Chrome()
+        browser = webdriver.Chrome(options=options)
     elif browser_name == 'firefox':
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
         print('\nstart firefox browser for test...')
-        browser = webdriver.Firefox()
+        browser = webdriver.Firefox(firefox_profile=fp)
     else:
         raise pytest.UsageError('--browser_name should be chrome or firefox')
     yield browser
--- test_parser.py
+++ test_parser.py
@@ -3,6 +3,9 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/"
 
 
+# link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
 def test_guest_should_see_login_link(browser):
     browser.get(link)
     browser.find_element(By.CSS_SELECTOR, "#login_link")
